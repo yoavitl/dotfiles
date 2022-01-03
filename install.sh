453979a2b5de8e551dffde304570ec7a826e7202
Yoav Tourgeman Levi <master.tavas@gmail.com>
2022-01-03 11:37:06 +0200

    Update install.sh
    changed the cask

--- install.sh
+++ install.sh
@@ -4,18 +4,17 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # brew install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew tap caskroom/cask
-brew tap caskroom/versions
+brew tap homebrew/cask
+brew tap homebrew/cask-versions
 brew install node
 brew install git
 brew install wget
-brew cask install atom
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install spotify
-brew cask install vlc
-brew cask install iterm2
-brew cask install karabiner-elements
-brew cask install 1clipboard
-brew cask install slack
-brew cask install keka
+brew install google-chrome
+brew install firefox
+brew install spotify
+brew install vlc
+brew install iterm2
+brew install karabiner-elements
+brew install 1clipboard
+brew install slack
+brew install keka
